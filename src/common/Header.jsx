46d06931959f2aa3f285ae80e92f99d233ff6317
nomalist <yojun926@naver.com>
2024-04-20 16:43:46 +0900

    React Route Add

---
+++ src/common/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  
+  return (
+    <>
+      <div>
+        <p>헤더입니다.</p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+
+  return (
+    <>
+      <div>
+        <p>메인 페이지 입니다.</p>
+      </div>
+    </>
+  )
+}
+
+export default Main;
