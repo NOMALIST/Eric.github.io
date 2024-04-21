52464f7cc9cec917b3e4ad3f9875d5ea36f1e6b6
nomalist <yojun926@naver.com>
2024-04-21 23:22:12 +0900

    Header Form add

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,13 +1,59 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
-const Header = () => {
-  
+const Header = (props) => {
+
+  //const { sections, title } = props;
+
+  const title = "블로그 제목";
+  const sections = "섹션";
+
   return (
-    <>
-      <div>
-        <p>헤더입니다.</p>
-      </div>
-    </>
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          {/* <SearchIcon /> */}
+        </IconButton>
+        <Button variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: "space-between", overflowX: "auto" }}
+      >
+        {/* {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))} */}
+      </Toolbar>
+    </React.Fragment>
   );
 };
 
